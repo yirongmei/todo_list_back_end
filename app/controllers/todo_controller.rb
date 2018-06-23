f9b135b6315e09000b78865b978020c50157607d
Yi Rong Mei <yirongmei@gmail.com>
2018-06-23 16:22:20 +0000

    Add instance variables to show page

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,2 +1,9 @@
 class TodoController < ApplicationController
+    def index
+    end
+    
+    def show
+      @description = "Buy workshop supplies"
+      @pomodoro = "3"
+    end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
+  
+  get '/todo/show', to: 'todo#show'
 end
