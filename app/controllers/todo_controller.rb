b7b1e27882e085fcf88fa6ada2f02bb40a2e2e3f
Yi Rong Mei <yirongmei@gmail.com>
2018-06-23 18:34:13 +0000

    Conditional Logic to show pages

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,7 +3,31 @@ class TodoController < ApplicationController
     end
     
     def show
-      @description = "Buy workshop supplies"
-      @pomodoro = "3"
+      if params[:id] == "1"
+        @description = "Make the curriculum"
+        @pomodoro = "4"
+    elsif params[:id] == "2"
+        @description = "Buy workshop supplies"
+        @pomodoro = "3"
+    elsif params[:id] == "3"
+        @description = "Meet with the volunteer trainers"
+        @pomodoro = "2"
+    elsif params[:id] == "4"
+        @description = "Order food for Saturday and Sunday"
+        @pomodoro = "1"
+    elsif params[:id] == "5"
+        @description = "Check pre-work assignments"
+        @pomodoro = "2"
+    elsif params[:id] == "6"
+        @description = "Send workshop location to all the students"
+        @pomodoro = "1"
+    elsif params[:id] == "7"
+        @description = "Have a great workshop"
+        @pomodoro = "0"
+    end
+
+        
+      
+    
     end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
   
-  get '/todo/show', to: 'todo#show'
+  get 'todo/show/:id', to: 'todo#show'
 end
