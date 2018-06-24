1414e39f679d1f46e9c75feb99642d567d5289c9
Yi Rong Mei <yirongmei@gmail.com>
2018-06-24 16:28:13 +0000

    Add update and delete actions

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,41 @@
 class TodoController < ApplicationController
-    def index
+  def new
+  end 
+  
+  def create
+    t = Todo.new
+    t.description = params[:description]
+    t.pomodoros = params[:pomodoros]
+    t.status = false
+    t.save 
+    redirect_to '/todo/index'
+  end 
+  
+  def index
       @todos = Todo.all
   end
     
   def show
     @todo = Todo.find(params[:id].to_i)
   end
+  
+  def edit
+    @todo = Todo.find(params[:id].to_i)
+  end 
+  
+  def update
+    todo = Todo.find(params[:id].to_i)
+    todo.description = params[:description]
+    todo.pomodoros = params[:pomodoros]
+    todo.save
+    
+    redirect_to "/todo/show/#{todo.id}"
+  end 
+  
+  def delete
+    todo = Todo.find(params[:id].to_i)
+    todo.destroy 
+      
+    redirect_to '/todo/index'
+  end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'todo/new', to: 'todo#new'
+  get 'todo/create', to: 'todo#create'
   get 'todo/index', to: 'todo#index'
-  
   get 'todo/show/:id', to: 'todo#show'
+  get 'todo/edit/:id', to: 'todo#edit'
+  get 'todo/update/:id', to: 'todo#update'
+  
+  get 'todo/delete/:id', to: 'todo#delete'
 end
